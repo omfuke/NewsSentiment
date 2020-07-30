b298cf1b77e3545f7711d0810b9b1aa5323d68c1
omfuke <46218908+omfuke@users.noreply.github.com>
2020-07-30 22:11:00 +0530

    selection on text

--- content.js
+++ content.js
@@ -1,17 +1,23 @@
 console.log("alert");
 
 $(document).ready(function () {
-  $("span.w_tle a").mouseover(function () {
+  $("li").click(function () {
     const text = $(this).text();
-    $(this).css("background-color", "yellow");
-    $.ajax({
-      type: "POST",
-      url: "http://localhost:5000/",
-      contentType: "application/json",
-      data: JSON.stringify({ text: text }),
-      success: function (response) {
-        console.log("Data Loaded: " + response.text + response.message);
-      },
-    });
+    var color = $(this).find("span.w_tle a").css("background-color");
+    $(this).find("span.w_tle a").css("background-color", "yellow");
+
+    if (color != "yellow") {
+      $.ajax({
+        type: "POST",
+        url: "http://localhost:5000/",
+        contentType: "application/json",
+        data: JSON.stringify({ text: text }),
+        success: function (response) {
+          console.log("Data Loaded: " + response.text + response.message);
+        },
+      });
+    }
   });
 });
+
+//span.w_tle a
