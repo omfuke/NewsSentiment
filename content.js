52fe6fa79e1705f3e55239c1ed89c5525c29d356
omfuke <46218908+omfuke@users.noreply.github.com>
2020-08-02 02:33:13 +0530

    Tested all sentimentss

--- content.js
+++ content.js
@@ -1,71 +1,70 @@
-// $(document).ready(function () {
-//   $("li").click(function () {
-//     const text = $(this).text();
-//     var color = $(this).find("span.w_tle a").css("background-color");
-//     $(this).find("span.w_tle a").css("background-color", "yellow");
-
-//     if (color != "yellow") {
-//       $.ajax({
-//         type: "POST",
-//         url: "http://localhost:5000/",
-//         contentType: "application/json",
-//         data: JSON.stringify({ text: text }),
-//         success: function (response) {
-//           console.log("Data Loaded: " + response.text + response.message);
-//         },
-//       });
-//     }
-//   });
-// });
-
 chrome.runtime.onMessage.addListener(gotMessage);
 
 function gotMessage(message, sender, sendResponse) {
-  console.log(message.txt);
+  //console.log(message.txt);
 
   if (message.txt === "hello") {
-    alert("Blue - NORMAL");
+    alert(
+      "Click on Yellow boxes\nBlue - POSITIVE\nRed - NEGATIVE\nGreen - NORMAL"
+    );
 
     $(document).ready(function () {
       $("li").mouseover(function () {
         const element = $(this);
         const text = $(this).text();
+        $("a").bind("click", false);
 
-        $(this).find("span.w_tle").addClass("red");
-
+        $(this).addClass("yellow");
         function handleData(response) {
           var sentiment = response.body.Sentiment;
           console.log(sentiment);
 
           if (sentiment === "NEGATIVE") {
-            element.css("background-color", "Tomato");
+            element.css({
+              "background-color": "Tomato",
+              "border-radius": "6%",
+            });
           }
           if (sentiment === "POSITIVE") {
-            element.css("background-color", "green");
+            element.css({
+              "background-color": "#3399ff",
+              "border-radius": "6%",
+            });
           }
           if (sentiment === "NEUTRAL") {
-            element.css("background-color", "blue");
+            element.css({ "background-color": "green", "border-radius": "6%" });
           }
         }
 
-        element.click(function () {
+        element.one("click", function () {
           $.ajax({
             type: "POST",
             url:
               "https://cexm3qgnp3.execute-api.us-east-1.amazonaws.com/dev/simple",
             contentType: "application/json",
-
+            crossDomain: true,
             data: JSON.stringify({ text: text }),
             success: handleData,
           });
         });
+        $(this)
+          .find("a")
+          .one("click", function () {
+            $.ajax({
+              type: "POST",
+              url:
+                "https://cexm3qgnp3.execute-api.us-east-1.amazonaws.com/dev/simple",
+              contentType: "application/json",
+              crossDomain: true,
+              data: JSON.stringify({ text: text }),
+              success: handleData,
+            });
+          });
       });
 
       $("li").mouseout(function () {
-        $(this).find("span.w_tle").removeClass("red");
+        $(this).removeClass("yellow");
       });
     });
   }
 }
-
-//span.w_tle a
